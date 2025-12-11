956bba8512719b803f17675517a24b265a2ecb56
Kooji0716 <930716jim@GMAIL.COM>
2025-12-11 14:36:32 +0800

    111

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def got():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
+    #app.run(debug=True)
 
 
